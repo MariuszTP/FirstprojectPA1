1f243e28fb390c98d87855b639a842f3592c79bd
MariuszTP <turekpython@gmail.com>
2022-03-20 22:17:28 +0100

    changes many

--- web/settings.py
+++ web/settings.py
@@ -127,12 +127,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'pageb/static/bpage/images')]
 
 MEDIA_URl = '/image/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/image')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pageb/static/bpage/images/')
+
+
+
+
+
+
+
+
+
+
 
 
 
